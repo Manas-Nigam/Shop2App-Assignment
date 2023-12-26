66ed4c17a9a074b531c8bcda20214846143132cb
Manas <manas.nigam@capitsign.com>
2023-12-27 01:52:47 +0530

    Added imports as a module and basic file setups done

---
+++ routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+const router = Router();
+
+
+router.post('/addProduct', async (req, res) => {
+    
+})
+
+export default router
--- server.js
+++ server.js
@@ -1,33 +1,16 @@
-const express = require("express");
-const axios = require("axios");
-require("dotenv").config();
+import express from "express";
+import shopifyRequest from "./utils.js";
+import router from "./routes.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-// All env variable here
 const PORT = process.env.PORT;
-const store_name = process.env.STORE;
-const version = process.env.VERSION;
 
 const shop2Appservice = express();
 
-
-const shopifyRequestUrl = `https://${store_name}.myshopify.com/admin/api/${version}/graphql.json`;
-
-
-// Shopify request function
-async function shopifyRequest(query, variables = {}) {
-  const response = axios.post(
-    shopifyRequestUrl,
-    { query, variables },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Shopify-Access-Token": process.env.ACCESS_TOKEN,
-      },
-    }
-  );
-  return response.data;
-}
+shop2Appservice.use(router);
 
 shop2Appservice.listen(PORT, () => {
+  shopifyRequest({}, {});
   console.log(`Server is running on port ${PORT}`);
 });
